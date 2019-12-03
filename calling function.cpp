544df1195542ca4f6df87e55b9d4372bb3c06cb0
Krutika Fanse <51167995+krutikafanse@users.noreply.github.com>
2019-12-03 11:54:53 -0500

    Create calling function.cpp

---
+++ calling function.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int addition (int x, int y)
+{
+  int r;
+  r=x*y;
+  return r;
+}
+
+int main ()
+{
+  int z;
+  z = multiply (5,3);
+  cout << "The result is " << z;
+}
