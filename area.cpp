af800151cb18912e9d37402ed9fcc0e23a453971
Krutika Fanse <51167995+krutikafanse@users.noreply.github.com>
2019-12-06 12:00:04 -0500

    Create area.cpp

---
+++ area.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+void area(int length,int breadth);            
+void area(float length, float breadth); 
+int main()
+{
+        int option=0;
+        int lengthi,breadthi;
+        float lengthf,breadthf;
+        std::cout<<"******Menu*******";
+        std::cout<<"\n1: Area using float values\n";
+        std::cout<<"\n2: Area using Integer values\n";
+        std::cin>>option;
+        switch(option)
+        {
+                case 1:std::cout<<"\nEnter the length and breadth of the rectangle\n";
+                           std::cin>>lengthf>>breadthf;
+                           area(lengthf,breadthf);
+                           break;
+                case 2:std::cout<<"\nEnter the length and breadth of the rectangle\n";
+                           std::cin>>lengthi>>breadthi;
+                           area(lengthi,breadthi);
+                           break;
+                default:std::cout<<"\n Invalid selection\n";
+        }
+}
+void area(int a,int b){
+                int areai;
+                areai=a*b;
+                std::cout<<"\nThe area of the rectangle is " <<areai;
+}
+void area(float a, float b){
+                float areaf;
+                areaf=a*b;
+                std::cout<<"\nThe area of the rectangle is " <<areaf;
+}
