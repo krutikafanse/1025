1f8f07ebcb05920e8c0e0135c8d520825c5f1e27
Krutika Fanse <51167995+krutikafanse@users.noreply.github.com>
2019-12-23 11:00:29 -0500

    Update LED.cpp

--- LED.cpp
+++ LED.cpp
@@ -8,7 +8,7 @@ void setup()
 void loop()
 {
   digitalWrite(ledPin, HIGH);   // sets the LED on
-  delay(1000);                  // waits for a second
+  delay(2000);                  // waits for a second
   digitalWrite(ledPin, LOW);    // sets the LED off
-  delay(1000);                  // waits for a second
+  delay(2000);                  // waits for a second
 }
