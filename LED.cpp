421bac72e78f8e8e90173bd70157d444cd0f278b
Krutika Fanse <51167995+krutikafanse@users.noreply.github.com>
2020-01-20 09:18:04 -0500

    Update LED.cpp

--- LED.cpp
+++ LED.cpp
@@ -2,7 +2,7 @@ int ledPin = 13;                 // LED connected to digital pin 13
 
 void setup()
 {
-  pinMode(ledPin, OUTPUT);      // sets the digital pin as output
+  pinMode(ledPin, Output);      // sets the digital pin as output
 }
 
 void loop()
